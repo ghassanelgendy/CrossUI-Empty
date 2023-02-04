17aba66c836b94bada5e9bec1690848ee71b89ff
ghassanelgendy <112765677+ghassanelgendy@users.noreply.github.com>
2023-02-04 04:51:47 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,16 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button3")
+                .setLeft("47.23809523809524em")
+                .setTop("40.38095238095238em")
+                .setCaption("Image Button")
+                .setImageClass("xui-icon-xui")
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
